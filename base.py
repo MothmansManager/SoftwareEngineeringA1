5b0dd63538e82f79bf7d868f22a402b377063262
Cian B. H <cianbodhihodnett@gmail.com>
2023-09-22 14:43:23 +0100

    Added ServerGen class
    Added a class that can create server objects with a server number and capacity.

---
+++ base.py
@@ -0,0 +1,16 @@
+#Creates servers with the server number and the capacity it can take for a job
+class serverGen:
+    def __init__(self, serverNum, capacity):
+        self.serverNum = serverNum
+        self.capacity = capacity
+        
+server1 = serverGen(1, 5)
+server2 = serverGen(2, 5)
+server3 = serverGen(3, 5)
+server4 = serverGen(4, 5)
+server5 = serverGen(5, 5)
+server6 = serverGen(6, 5)
+server7 = serverGen(7, 5)
+server8 = serverGen(8, 5)
+server9 = serverGen(9, 5)
+server10 = serverGen(10, 5)
