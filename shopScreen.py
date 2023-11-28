ffebf29691101d70ed2c75cd05e20c0102cf288d
Cian B. H <cianbodhihodnett@gmail.com>
2023-11-28 21:09:21 +0000

    Update to colours in shop, more selection + bug fixes
    Added a total of four applicable colours to shop
    fixed buttons appearing where they shouldn't/ incorrect screen clears
    Now there is a preview of shop colours

--- shopScreen.py
+++ shopScreen.py
@@ -37,50 +37,115 @@ def shopScreen():
     colourHeader.place(relx=.5, rely=.15,anchor= CENTER)
     settings.widgetList.append(colourHeader)
 
-    optionOnetxt = tk.Label( text="Baby Blue Theme",
+    optionOnetxt = tk.Label( text="Ice King",
                         fg = "black",
                         bg = settings.bgColor,
-                        font = ("Segoe UI", 25))
+                        font = ("Segoe UI", 13))
     optionOnetxt.place(relx=.25, rely=.2,anchor= CENTER)
     settings.widgetList.append(optionOnetxt)
+
+    option1Color1 = tk.Label( text="      ",
+                        bg = "#99b3ff",
+                        font = ("segoue ui", 30))
+    option1Color1.place(relx=.20, rely=.25,anchor= CENTER)
+    settings.widgetList.append(option1Color1)
+
+    option1Color2 = tk.Label( text="      ",
+                        bg = "#99ffe6",
+                        font = ("segoue ui", 30))
+    option1Color2.place(relx=.32, rely=.25,anchor= CENTER)
+    settings.widgetList.append(option1Color2)
 
     optionOne =tk.Button(text="Buy",
                             fg = "black",
                             bg = settings.bgColor,
                             command = lambda: [changeColor("#99b3ff", "#99ffe6"), gui.clearScreen(taskBar.tbWidgets), gui.clearScreen(settings.widgetList), shopScreen(), taskBar.taskbar()])
-    optionOne.place(relx=.25, rely=.25,anchor= CENTER)
+    optionOne.place(relx=.25, rely=.3,anchor= CENTER)
     settings.widgetList.append(optionOne)
 
     optionTwotxt = tk.Label( text="Forest Walk Theme",
                         fg = "black",
                         bg = settings.bgColor,
-                        font = ("Segoe UI", 25))
-    optionTwotxt.place(relx=.25, rely=.2,anchor= CENTER)
-    settings.widgetList.append(optionOnetxt)
+                        font = ("Segoe UI", 13))
+    optionTwotxt.place(relx=.75, rely=.2,anchor= CENTER)
+    settings.widgetList.append(optionTwotxt)
+
+    option2Color1 = tk.Label( text="      ",
+                        bg = "#4a6741",
+                        font = ("segoue ui", 30))
+    option2Color1.place(relx=.68, rely=.25,anchor= CENTER)
+    settings.widgetList.append(option2Color1)
+
+    option2Color2 = tk.Label( text="      ",
+                        bg = "#3f5a36",
+                        font = ("segoue ui", 30))
+    option2Color2.place(relx=.80, rely=.25,anchor= CENTER)
+    settings.widgetList.append(option2Color2)
 
     optionTwo =tk.Button(text="Buy",
                             fg = "black",
                             bg = settings.bgColor,
                             command = lambda: [changeColor("#4a6741", "#3f5a36"), gui.clearScreen(taskBar.tbWidgets), gui.clearScreen(settings.widgetList), shopScreen(), taskBar.taskbar()])
-    optionTwo.place(relx=.75, rely=.2,anchor= CENTER)
+    optionTwo.place(relx=.75, rely=.3,anchor= CENTER)
     settings.widgetList.append(optionTwo)
+
+    optionThreetxt = tk.Label( text="Yellow Meadows",
+                        fg = "black",
+                        bg = settings.bgColor,
+                        font = ("Segoe UI", 13))
+    optionThreetxt.place(relx=.25, rely=.45,anchor= CENTER)
+    settings.widgetList.append(optionThreetxt)
+
+    option3Color1 = tk.Label( text="      ",
+                        bg = "#ffd800",
+                        font = ("segoue ui", 30))
+    option3Color1.place(relx=.2, rely=.50,anchor= CENTER)
+    settings.widgetList.append(option3Color1)
+
+    option3Color2 = tk.Label( text="      ",
+                        bg = "#ffbf00",
+                        font = ("segoue ui", 30))
+    option3Color2.place(relx=.32, rely=.50,anchor= CENTER)
+    settings.widgetList.append(option3Color2)
 
     optionThree =tk.Button(text="Buy",
                             fg = "black",
                             bg = settings.bgColor,
-                            command = lambda: [changeColor("green", "red"), gui.clearScreen(taskBar.tbWidgets), gui.clearScreen(settings.widgetList), shopScreen(), taskBar.taskbar()])
-    optionThree.place(relx=.25, rely=.4,anchor= CENTER)
+                            command = lambda: [changeColor("#ffd800", "#ffbf00"), gui.clearScreen(taskBar.tbWidgets), gui.clearScreen(settings.widgetList), shopScreen(), taskBar.taskbar()])
+    optionThree.place(relx=.25, rely=.55,anchor= CENTER)
     settings.widgetList.append(optionThree)
+
+    optionFourtxt = tk.Label( text="50 Shades of Beige",
+                        fg = "black",
+                        bg = settings.bgColor,
+                        font = ("Segoe UI", 13))
+    optionFourtxt.place(relx=.75, rely=.45,anchor= CENTER)
+    settings.widgetList.append(optionFourtxt)
+
+    option4Color1 = tk.Label( text="      ",
+                        bg = "#969286",
+                        font = ("segoue ui", 30))
+    option4Color1.place(relx=.68, rely=.50,anchor= CENTER)
+    settings.widgetList.append(option4Color1)
+
+    option4Color2 = tk.Label( text="      ",
+                        bg = "#e3ded1",
+                        font = ("segoue ui", 30))
+    option4Color2.place(relx=.80, rely=.50,anchor= CENTER)
+    settings.widgetList.append(option4Color2)
+    
 
     optionFour =tk.Button(text="Buy",
                             fg = "black",
                             bg = settings.bgColor,
-                            command = lambda: [changeColor("green", "red"), gui.clearScreen(taskBar.tbWidgets), gui.clearScreen(settings.widgetList), shopScreen(), taskBar.taskbar()])
-    optionFour.place(relx=.75, rely=.4,anchor= CENTER)
+                            command = lambda: [changeColor("#969286", "#e3ded1"), gui.clearScreen(taskBar.tbWidgets), gui.clearScreen(settings.widgetList), shopScreen(), taskBar.taskbar()])
+    optionFour.place(relx=.75, rely=.55,anchor= CENTER)
+    settings.widgetList.append(optionFour)
+
 
     resetColor =tk.Button(text="Reset Colours",
                             fg = "black",
                             bg = settings.bgColor,
                             command = lambda: [changeColor("pink", "#ffe2e6"), gui.clearScreen(taskBar.tbWidgets), gui.clearScreen(settings.widgetList), shopScreen(), taskBar.taskbar()])
     resetColor.place(relx=.5, rely=.8,anchor= CENTER)
-    settings.widgetList.append(optionFour)
+    settings.widgetList.append(resetColor)
